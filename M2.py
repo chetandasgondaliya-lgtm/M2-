eb8528c43ecef3ebeb257c7591e10e594e4b2786
chetandasgondaliya-lgtm <chetandasgondaliya@gmail.com>
2025-08-31 11:54:27 +0530

    Add files via upload

---
+++ M2.py
@@ -0,0 +1,15 @@
+a = float(input("enter first number , "))
+b = float(input("enter second number , "))
+
+print("addition:", a + b ,)
+print("subtraction:", a - b ,)
+print("multiplication:" ,a * b )
+print("division:" , a /b )
+
+#TASK 2
+
+first_name = input("enter your name: ")
+last_name = input("enter your last name: ")
+full_name = first_name +" "+ last_name
+
+print("hello, " + full_name + " welcome to python program.")
